170d9316f979148e78a413b71c9eb8546c17a295
hoa-uit <19521518@gm.uit.edu.vn>
2024-05-25 23:14:51 +0700

    [DFS] find all path sum is S

---
+++ depth-first-search/findNumberRootToLeafPathSumIsInputValue.js
@@ -0,0 +1,62 @@
+/*
+    given a binary tree, find all the path from root to leaf that sum node values is S (input integer number)
+
+    use recursion to traverse all the nodes in tree in root to leaf way
+
+    recurse until we reach leaf node, and sum is S, we count it. otherwise, we ignore
+*/
+
+class TreeNode {
+    constructor(value, leftNode = null, rightNode = null) {
+        this.value = value;
+        this.leftNode = leftNode;
+        this.rightNode = rightNode;
+    }
+}
+
+const root = new TreeNode(1);
+root.leftNode = new TreeNode(2);
+root.rightNode = new TreeNode(3);
+root.leftNode.leftNode = new TreeNode(4);
+root.leftNode.rightNode = new TreeNode(5);
+root.rightNode.leftNode = new TreeNode(6);
+root.rightNode.rightNode = new TreeNode(6);
+root.leftNode.leftNode.leftNode = new TreeNode(8);
+
+
+const countPath = (node, sum, pathValues, paths) => {
+    if (node == null) {
+        return;
+    }
+    console.log('node: ', node.value);
+    pathValues.push(node.value);
+    console.log('pathValues: ', pathValues);
+    if (node.value == sum && node.leftNode == null && node.rightNode == null) {
+        count++;
+        paths.push([...pathValues]);
+    }
+    else {
+        countPath(node.leftNode, sum - node.value, pathValues, paths);
+        countPath(node.rightNode, sum - node.value, pathValues, paths);
+
+    }
+
+    pathValues.pop();
+};
+
+
+//       1
+//     /  \
+//    2    3  
+//   / \  / \
+//  4   5 6  6
+// /
+//8
+
+let count = 0;
+const paths = [];
+const pathValues = [];
+
+countPath(root, 10, pathValues, paths);
+console.log(count);
+console.log(paths);
